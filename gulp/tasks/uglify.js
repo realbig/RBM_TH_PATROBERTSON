9422e60224190f29c61a192c60ed8da8da5c3e96
Eric Defore <eric.defore@gmail.com>
2016-09-07 15:11:15 -0400

    Init Commit

---
+++ gulp/tasks/uglify.js
@@ -0,0 +1,68 @@
+var $             = require( 'gulp-load-plugins' )();
+var config        = require( '../util/loadConfig' ).javascript;
+var gulp          = require( 'gulp' );
+var foreach       = require( 'gulp-foreach' );
+var notify        = require( 'gulp-notify' );
+var fs            = require( 'fs' );
+var pkg           = JSON.parse( fs.readFileSync( './package.json' ) );
+var onError       = notify.onError( {
+   title:    pkg.name,
+   message:  '<%= error.name %> <%= error.message %>'   
+} );
+
+gulp.task( 'front-uglify', function() {
+
+    return gulp.src( config.front.src )
+        .pipe( $.plumber( { errorHandler: onError } ) )
+        .pipe( $.sourcemaps.init() )
+        .pipe( $.babel() )
+        .pipe( $.concat( config.front.filename ) )
+        .pipe( $.uglify() )
+        .pipe( $.sourcemaps.write( '.' ) )
+        .pipe( gulp.dest( config.dest.root ) )
+        .pipe( $.plumber.stop() )
+        .pipe( notify( {
+            title: pkg.name,
+            message: 'JS Complete'
+        } ) );
+
+} );
+
+gulp.task( 'admin-uglify', function() {
+
+    return gulp.src( config.admin.bowerPaths.concat( config.admin.src ) )
+        .pipe( $.plumber( { errorHandler: onError } ) )
+        .pipe( $.sourcemaps.init() )
+        .pipe( $.babel() )
+        .pipe( $.concat( config.admin.filename ) )
+        .pipe( $.uglify() )
+        .pipe( $.sourcemaps.write( '.' ) )
+        .pipe( gulp.dest( config.dest.root ) )
+        .pipe( $.plumber.stop() )
+        .pipe( notify( {
+            title: pkg.name,
+            message: 'Admin JS Complete'
+        } ) );
+
+} );
+
+gulp.task( 'tinymce-uglify', function() {
+
+    return gulp.src( config.tinymce.src )
+        .pipe( foreach( function( stream, file ) {
+            return stream
+                .pipe( $.plumber( { errorHandler: onError } ) )
+                .pipe( $.babel() )
+                .pipe( $.uglify() )
+                .pipe( gulp.dest( config.tinymce.dest ) )
+                .pipe( $.plumber.stop() )
+        } ) )
+        .pipe( notify( {
+            title: pkg.name,
+            message: 'TinyMCE JS Complete'
+        } ) );
+
+} );
+
+gulp.task( 'uglify', ['front-uglify', 'admin-uglify', 'tinymce-uglify'], function( done ) {
+} );
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,11 @@
+var config      = require( '../util/loadConfig' ).watch;
+var gulp        = require( 'gulp' );
+
+// Watch files for changes, recompile/rebuild
+gulp.task( 'watch', function() {
+    gulp.watch( config.javascript.front, ['front-uglify'] );
+    gulp.watch( config.javascript.admin, ['admin-uglify'] );
+    gulp.watch( config.javascript.tinymce, ['tinymce-uglify'] );
+    gulp.watch( config.sass.front, ['front-sass'] );
+    gulp.watch( config.sass.admin, ['admin-sass'] );
+} );
