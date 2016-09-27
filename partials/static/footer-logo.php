1dfdd584a44c01e912b749a13fdd13731a843864
Eric Defore <eric.defore@gmail.com>
2016-09-27 09:15:37 -0400

    Move Header/Footer logo into partials

---
+++ partials/static/footer-logo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Footer Banner Image
+ *
+ * @since   1.0.0
+ * @package PatRobertson
+ */
+
+?>
+
+<div class="footer-logo" style="background-image: url( '<?php echo wp_get_attachment_image_url( get_theme_mod( 'pat_footer_image', 1 ), 'full' ); ?>' );">
+                        
+    <div class="footer-content">
+        <?php echo apply_filters( 'the_content', get_theme_mod( 'pat_footer_text', _x( '<h1>When life knocks you down <br />get up and dance!</h1>', 'Default Customizer Footer Text', THEME_ID ) ) ); ?>
+    </div>
+
+</div>
