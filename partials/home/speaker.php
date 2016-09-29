bab42b07b9812579cd42d1da7dd5aa7fc254969e
Eric Defore <eric.defore@gmail.com>
2016-09-29 13:12:27 -0400

    Not ideal, but it works

--- partials/home/speaker.php
+++ partials/home/speaker.php
@@ -10,21 +10,21 @@
 
 <div class="pat-speaker row expanded">
     
-    <div class="small-12 medium-6 medium-push-3 columns text-container">
+    <div class="small-12 medium-7 medium-push-2 columns text-container">
 
         <h1><?php echo get_post_meta( get_the_ID(), '_rbm_pat_home_speaker_title', true ); ?></h1>
 
     </div>
 
-    <div class="small-12 medium-3 medium-pull-6 columns megaphone-container">
+    <div class="small-12 medium-2 medium-pull-7 columns megaphone-container">
 
         <span class="fa fa-4x fa-bullhorn"></span>
 
     </div>
 
-    <div class="small-12 medium-3 medium-pull-6 columns button-container">
+    <div class="small-12 medium-3 medium-pull-7 columns button-container">
 
-        <a class="secondary hollow button" href="<?php echo get_post_meta( get_the_ID(), '_rbm_path_home_speaker_button_link', true ); ?>">
+        <a class="secondary large hollow button" href="<?php echo get_post_meta( get_the_ID(), '_rbm_path_home_speaker_button_link', true ); ?>">
             <?php echo get_post_meta( get_the_ID(), '_rbm_pat_home_speaker_button_text', true ); ?>
         </a>
 
