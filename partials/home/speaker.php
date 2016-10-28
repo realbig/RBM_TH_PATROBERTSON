cbd7c25b4e075d0e4781dd586437a0bca008e71a
Eric Defore <eric.defore@gmail.com>
2016-10-28 15:35:03 -0400

    Speaker Section Overlap on iPad. Closes #18

--- partials/home/speaker.php
+++ partials/home/speaker.php
@@ -13,19 +13,19 @@ defined( 'ABSPATH' ) || die();
 
 <div class="pat-speaker row expanded">
     
-    <div class="small-12 medium-7 medium-push-2 columns text-container">
+    <div class="small-12 medium-5 medium-push-2 large-7 large-push-2 columns text-container">
 
         <h1><?php echo get_post_meta( get_the_ID(), '_rbm_pat_home_speaker_title', true ); ?></h1>
 
     </div>
 
-    <div class="small-12 medium-2 medium-pull-7 columns megaphone-container">
+    <div class="small-12 medium-2 medium-pull-5 large-2 large-pull-7 columns megaphone-container">
 
         <span class="fa fa-4x fa-bullhorn"></span>
 
     </div>
 
-    <div class="small-12 medium-3 medium-pull-7 columns button-container">
+    <div class="small-12 medium-4 medium-pull-5 large-3 large-pull-7 columns button-container">
 
         <a class="secondary large hollow button" data-open="booking-modal">
             <?php echo get_post_meta( get_the_ID(), '_rbm_pat_home_speaker_button_text', true ); ?>
@@ -33,7 +33,7 @@ defined( 'ABSPATH' ) || die();
 
     </div>
     
-    <div class="small-12 medium-3 columns image-container">
+    <div class="small-12 medium-5 large-3 columns image-container">
 
         <?php echo wp_get_attachment_image( get_post_meta( get_the_ID(), '_rbm_pat_home_speaker_image', true ), 'medium' ); ?>
 
