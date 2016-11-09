71729c68d82a51dcb3a463ca580eefc1bd38d9b2
Eric Defore <eric.defore@gmail.com>
2016-11-09 14:34:11 -0500

    Sizing Adjustments for About Section. Closes #21

--- partials/home/about.php
+++ partials/home/about.php
@@ -13,13 +13,13 @@ defined( 'ABSPATH' ) || die();
 
 <div class="pat-about row expanded" data-equalizer data-equalize-on="medium">
 
-    <div class="small-12 medium-6 medium-push-6 large-3 large-push-9 columns image-container" data-equalizer-watch>
+    <div class="small-12 medium-6 medium-push-6 columns image-container" data-equalizer-watch>
 
         <?php echo wp_get_attachment_image( get_post_meta( get_the_ID(), '_rbm_pat_home_about_image', true ), 'full' ); ?>
 
     </div>
 
-    <div class="small-12 medium-6 medium-pull-6 large-9 large-pull-3 columns text-container" data-equalizer-watch>
+    <div class="small-12 medium-6 medium-pull-6 columns text-container" data-equalizer-watch>
 
         <h1><?php echo get_post_meta( get_the_ID(), '_rbm_pat_home_about_title', true ); ?></h1>
 
